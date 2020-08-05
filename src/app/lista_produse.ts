c9293b358981419c62e268b9661f7545d64dfe13
malinabenegui <malina.benegui@gmail.com>
2020-08-05 09:12:43 +0300

    Added products

--- src/app/lista_produse.ts
+++ src/app/lista_produse.ts
@@ -3,99 +3,283 @@ import {Produs} from './structura_produs';
 export const listaProduse: Produs[] = [
     {
         productCode: 'CD93214121',
-        urlImage: ['../../../assets/gard_oval.jpeg'],
-        description: 'Este solutia perfecta pentru consolidarea solului si intarirea terenurilor situate in panta sau pentru amenajarea parcarilor. Forma speciala a acestui produs permite decorarea cu verdeata, imbinand utilul cu frumosul pentru obtinerea unui efect estetic deosebit.',
-        subTitle: 'Gard frumos fara  forme',
-        name: 'Gard de beton oval',
+        urlImage: ['../../../assets/products/gard_valuri.jpg'],
+        description: 'Placi produse din ciment performant, armate cu fier beton Ø 6 ce ofera o rezistenta superioara perfecte pentru ingradirea curtii',
+        name: 'Gard de beton 3D',
         details: 'detalii',
-        height: '150',
-        width: '150',
-        price: '100',
+        height: '150/200',
+        width: '-',
+        length: '200',
+        price: '110',
         dimensionUnitMeasure: 'cm',
         priceUnitMeasure: 'Lei / mL'
     },
 
     {
         productCode: 'CD93214121',
-        urlImage: ['../../../assets/gard_lemn.jpeg'],
-        description: 'Gardul nostru este solutia perfecta pentru curtea ta, forma si materialul acestuia imbina frumosul cu utilul intr-un mod splendid.',
-        subTitle: 'Gard frumos',
-        name: 'Gard de beton lemn',
+        urlImage: ['../../../assets/products/gard_timurlea2.jpg'],
+        description: 'Placi produse din ciment performant, armate cu fier beton Ø 6 ce ofera o rezistenta superioara perfecte pentru ingradirea curtii',
+        name: 'Gard de beton Timurlea 2',
         details: 'detalii',
-        height: '150',
-        width: '150',
-        price: '100',
+        height: '150/200',
+        width: '-',
+        length: '200',
+        price: '110',
         dimensionUnitMeasure: 'cm',
         priceUnitMeasure: 'Lei / mL'
     },
 
     {
         productCode: 'CD93214121',
-        urlImage: ['../../../assets/gard_timurla.jpeg'],
-        description: 'Gardul nostru este solutia perfecta pentru curtea ta, forma si materialul acestuia imbina frumosul cu utilul intr-un mod splendid.',
-        subTitle: 'Gard frumos',
-        name: 'Gard de beton oval',
+        urlImage: ['../../../assets/products/gard_lemn.jpg'],
+        description: 'Placi produse din ciment performant, armate cu fier beton Ø 6 ce ofera o rezistenta superioara perfecte pentru ingradirea curtii',
+        name: 'Gard de beton Lemn Suprapus',
         details: 'detalii',
-        height: '150',
-        width: '150',
-        price: '100',
+        height: '150/180',
+        width: '-',
+        length: '190',
+        price: '230',
         dimensionUnitMeasure: 'cm',
-        priceUnitMeasure: 'Lei / mL'
+        priceUnitMeasure: 'Lei / lacra'
     },
 
     {
         productCode: 'CD93214121',
-        urlImage: ['../../../assets/gard_timurla.jpeg'],
-        description: 'Gardul nostru este solutia perfecta pentru curtea ta, forma si materialul acestuia imbina frumosul cu utilul intr-un mod splendid.',
-        subTitle: 'Gard frumos',
-        name: 'Gard de beton oval',
+        urlImage: ['../../../assets/products/boltari20.jpg', '../../../assets/products/boltari12.jpg'],
+        description: 'Solutia perfecta pentru constructia peretilor si a gardurilor. Sunt produsi din ciment performant ce le ofera o rezistenta superioara',
+        name: 'Boltari',
         details: 'detalii',
-        height: '150',
-        width: '150',
-        price: '100',
+        height: '20',
+        width: '12/15/20',
+        length: '40',
+        price: '2,30',
         dimensionUnitMeasure: 'cm',
-        priceUnitMeasure: 'Lei / mL'
+        priceUnitMeasure: 'Lei / buc'
     },
 
     {
         productCode: 'CD93214121',
-        urlImage: ['../../../assets/gard_timurla.jpeg'],
-        description: 'Gardul nostru este solutia perfecta pentru curtea ta, forma si materialul acestuia imbina frumosul cu utilul intr-un mod splendid.',
-        subTitle: 'Gard frumos',
-        name: 'Gard de beton oval',
+        urlImage: ['../../../assets/products/capac_neted_gri.jpg', '../../../assets/products/capac_floare_gri.jpg',
+            '../../../assets/products/capac_bob_rosu.jpg', '../../../assets/products/capac_ascutit.rosu.jpg'],
+        description: 'Produse din ciment performant, cu o rezistenta superioara, capacele constituie ' +
+            'un accesoriu indispensabil pentru gard',
+        name: 'Capac stalp gard bob',
         details: 'detalii',
-        height: '150',
-        width: '150',
-        price: '100',
+        height: '-',
+        width: '30/35/40/45/50',
+        length: '30/35/40/45/50',
+        price: '14',
         dimensionUnitMeasure: 'cm',
-        priceUnitMeasure: 'Lei / mL'
+        priceUnitMeasure: 'Lei / buc'
     },
 
     {
         productCode: 'CD93214121',
-        urlImage: ['../../../assets/gard_timurla.jpeg'],
-        description: 'Este solutia perfecta pentru consolidarea solului si intarirea terenurilor situate in panta sau pentru amenajarea parcarilor. Forma speciala a acestui produs permite decorarea cu verdeata, imbinand utilul cu frumosul pentru obtinerea unui efect estetic deosebit.',
-        subTitle: 'Gard frumos',
-        name: 'Gard de beton oval',
+        urlImage: ['../../../assets/products/capac_neted_gri.jpg', '../../../assets/products/capac_floare_gri.jpg',
+            '../../../assets/products/capac_bob_rosu.jpg', '../../../assets/products/capac_ascutit.rosu.jpg'],
+        description: 'Produse din ciment performant, cu o rezistenta superioara, capacele constituie ' +
+            'un accesoriu indispensabil pentru gard',
+        name: 'Capac stalp gard neted',
         details: 'detalii',
-        height: '150',
-        width: '150',
-        price: '100',
+        height: '-',
+        width: '30/35/40/45/50',
+        length: '30/35/40/45/50',
+        price: '14',
         dimensionUnitMeasure: 'cm',
-        priceUnitMeasure: 'Lei / mL'
+        priceUnitMeasure: 'Lei / buc'
     },
 
     {
         productCode: 'CD93214121',
-        urlImage: ['../../../assets/gard_timurla.jpeg'],
-        description: 'Este solutia perfecta pentru consolidarea solului si intarirea terenurilor situate in panta sau pentru amenajarea parcarilor. Forma speciala a acestui produs permite decorarea cu verdeata, imbinand utilul cu frumosul pentru obtinerea unui efect estetic deosebit.',
-        subTitle: 'Gard frumos',
-        name: 'Fantana arteziana',
+        urlImage: ['../../../assets/products/capac_neted_gri.jpg', '../../../assets/products/capac_floare_gri.jpg',
+            '../../../assets/products/capac_bob_rosu.jpg', '../../../assets/products/capac_ascutit.rosu.jpg'],
+        description: 'Produse din ciment performant, cu o rezistenta superioara, capacele constituie ' +
+            'un accesoriu indispensabil pentru gard',
+        name: 'Capac stalp gard floare',
         details: 'detalii',
-        height: '150',
-        width: '150',
-        price: '100',
+        height: '-',
+        width: '30/35/40/45/50',
+        length: '30/35/40/45/50',
+        price: '14',
         dimensionUnitMeasure: 'cm',
-        priceUnitMeasure: 'Lei / mL'
+        priceUnitMeasure: 'Lei / buc'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/coama_50x30.jpg', '../../../assets/products/coama_50x30_rosu.jpg',
+            '../../../assets/products/coama_40x27.jpg', '../../../assets/products/coama_40x27_rosu.jpg'],
+        description: 'Produse din ciment performant, cu o rezistenta superioara, coamele constituie ' +
+            'un accesoriu indispensabil pentru gard',
+        name: 'Coama gard neted',
+        details: 'detalii',
+        height: '-',
+        width: '30',
+        length: '50',
+        price: '15/17',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / buc'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/coama_40x35_bob.jpg', '../../../assets/products/coama_40x35_bob_rosu.jpg'],
+        description: 'Produse din ciment performant, cu o rezistenta superioara, coamele constituie ' +
+            'un accesoriu indispensabil pentru gard',
+        name: 'Coama gard bob',
+        details: 'detalii',
+        height: '-',
+        width: '27',
+        length: '40',
+        price: '12/14',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / buc'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/coama_lunga.jpg'],
+        description: 'Produse din ciment performant, cu o rezistenta superioara, coamele constituie ' +
+            'un accesoriu indispensabil pentru gard',
+        name: 'Coama gard lunga',
+        details: 'detalii',
+        height: '-',
+        width: '19',
+        length: '190',
+        price: '35',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / buc'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/fantana_arteziana_mica.png'],
+        description: 'Fantana arteziana creeaza o atmosfera deosebita in gradina dumneavoastra. ' +
+            'Sunetul linistitor al apei va ofera senzatia de calm si echilibru.',
+        name: 'Fantana Arteziana mica',
+        details: 'detalii',
+        height: '170',
+        width: '90',
+        length: '-',
+        price: '900',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/fantana_arteziana_mare.jpg'],
+        description: 'Fantana arteziana creeaza o atmosfera deosebita in gradina dumneavoastra. ' +
+            'Sunetul linistitor al apei va ofera senzatia de calm si echilibru.',
+        name: 'Fantana Arteziana mare',
+        details: 'detalii',
+        height: '190',
+        width: '200',
+        length: '-',
+        price: '2.500',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/pavele_harta_gri.jpg', '../../../assets/products/pavele_harta_rosu.jpg'],
+        description: 'Produse din ciment performant ce le ofera o rezistenta superioara, pavelele model Harta' +
+            'reprezinta solutia perfecta pentru pavarea pietonala',
+        name: 'Pavele Harta',
+        details: 'detalii',
+        height: '3',
+        width: '-',
+        length: '-',
+        price: '33',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / mp'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/pavele_romanita.jpg'],
+        description: 'Produse din ciment performant ce le ofera o rezistenta superioara, pavelele model Floral' +
+            'reprezinta solutia perfecta de pavaj rutier',
+        name: 'Pavele Florale',
+        details: 'detalii',
+        height: '4.5',
+        width: '-',
+        length: '-',
+        price: '45',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / mp'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/pavele_romanita.jpg'],
+        description: 'Produse din ciment performant ce le ofera o rezistenta superioara, pavelele model Lespede' +
+            'reprezinta solutia perfecta de pavaj rutier',
+        name: 'Pavele lespede',
+        details: 'detalii',
+        height: '5',
+        width: '-',
+        length: '-',
+        price: '10',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / buc'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/pavele_yeti_gri.jpg', '../../../assets/products/pavele_yeti_rosu.jpg'],
+        description: 'Produse din ciment performant ce le ofera o rezistenta superioara, pavelele model Yeti' +
+            'reprezinta solutia perfecta de pavaj pietonal',
+        name: 'Pavele Yeti',
+        details: 'detalii',
+        height: '-',
+        width: '-',
+        length: '-',
+        price: '13',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / buc'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/piatra_z.jpg'],
+        description: 'Elemente pentru placarea fatadelor exterioare',
+        name: 'Piatra decorativa Z',
+        details: 'detalii',
+        height: '-',
+        width: '-',
+        length: '-',
+        price: '35',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / buc'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/piatra_p.jpg'],
+        description: 'Elemente pentru placarea fatadelor exterioare',
+        name: 'Piatra decorativa P',
+        details: 'detalii',
+        height: '-',
+        width: '-',
+        length: '-',
+        price: '33',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / buc'
+    },
+
+    {
+        productCode: 'CD93214121',
+        urlImage: ['../../../assets/products/piatra_d.jpg'],
+        description: 'Elemente pentru placarea fatadelor exterioare',
+        name: 'Piatra decorativa D',
+        details: 'detalii',
+        height: '-',
+        width: '-',
+        length: '-',
+        price: '33',
+        dimensionUnitMeasure: 'cm',
+        priceUnitMeasure: 'Lei / buc'
     },
 ];
--- src/app/structura_produs.ts
+++ src/app/structura_produs.ts
@@ -2,11 +2,11 @@ export interface Produs {
     productCode: string;
     urlImage: string[];
     name: string;
-    subTitle: string;
     description: string;
     details: string;
     height: string;
     width: string;
+    length: string;
     price: string;
     dimensionUnitMeasure: string;
     priceUnitMeasure: string;
